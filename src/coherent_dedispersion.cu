e5b18d975e5ecfc1b149fe2aef5254da437a3cf9
Sam McSweeney <sammy.mcsweeney@gmail.com>
2022-03-19 20:11:44 +0800

    Switched to the glfw minimal example, and started adding mouse interactivity.

--- src/coherent_dedispersion.cu
+++ src/coherent_dedispersion.cu
@@ -2,14 +2,22 @@
 #include <cuComplex.h>
 #include "../src/cudaErrorChecking.h"
 
-#include <GL/gl.h>
-#include <GL/glut.h>
+//#include <GL/gl.h>
+//#include <GL/glut.h>
+
+#include <GL/glew.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
 
 #include <stdlib.h>
 #include <stdio.h>
 
 // GLUT-related constants
 #define OPEN_FILE  1
+
+// Mouse position
+static double xpos;
+static double ypos;
 
 /**
  * Convert a VDIF buffer into an array of floats
@@ -115,8 +123,8 @@ void display()
 }
 
 
-
-int main( int argc, char **argv )
+/*
+int glut_main( int argc, char **argv )
 {
     // Use a single buffered window in RGB mode (as opposed to a double-buffered
     // window or color-index mode).
@@ -179,4 +187,116 @@ int main( int argc, char **argv )
     gpuErrchk( cudaFree( d_vdifData ) );
     gpuErrchk( cudaFree( d_StokesI ) );
     gpuErrchk( cudaFreeHost( vdif ) );
+}
+*/
+
+void mouse_button_callback( GLFWwindow *window, int button, int action, int mods )
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        glfwGetCursorPos( window, &xpos, &ypos );
+        fprintf( stderr, "(x, y) = (%lf, %lf)\n", xpos, ypos );
+    }
+}
+
+int main( int argc, char *argv[] )
+{
+    // Start GL context and O/S window using the GLFW helper library
+    glfwInit();
+    const char *glfwerr;
+    int code = glfwGetError( &glfwerr );
+    if (code != GLFW_NO_ERROR)
+    {
+        fprintf( stderr, "ERROR: could not start GLFW3: %s\n", glfwerr );
+        return EXIT_FAILURE;
+    }
+
+    GLFWwindow* window = glfwCreateWindow( 640, 480, "Hello Triangle", NULL, NULL );
+    if (!window)
+    {
+        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent( window );
+    glfwSetMouseButtonCallback( window, mouse_button_callback );
+
+    // Start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // Get version info
+    const GLubyte* renderer = glGetString( GL_RENDERER ); // get renderer string
+    const GLubyte* version = glGetString( GL_VERSION ); // version as a string
+    printf( "Renderer: %s\n", renderer );
+    printf( "OpenGL version supported %s\n", version );
+
+    // Tell GL to only draw onto a pixel if the shape is closer to the viewer
+    glEnable( GL_DEPTH_TEST ); // enable depth-testing
+    glDepthFunc( GL_LESS ); // depth-testing interprets a smaller value as "closer"
+
+    // Define a triangle
+    float points[] = {
+        0.5f,  0.5f,  0.0f,
+        0.5f, -0.5f,  0.0f,
+        -0.5f, 0.5f,  0.0f,
+        -0.5f, -0.5f,  0.0f
+    };
+
+    GLuint vbo = 0;
+    glGenBuffers( 1, &vbo );
+    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+    glBufferData( GL_ARRAY_BUFFER, 12 * sizeof(float), points, GL_STATIC_DRAW );
+
+    GLuint vao = 0;
+    glGenVertexArrays( 1, &vao );
+    glBindVertexArray( vao );
+    glEnableVertexAttribArray( 0 );
+    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+
+    const char* vertex_shader =
+        "#version 400\n"
+        "in vec3 vp;"
+        "void main() {"
+        "  gl_Position = vec4(vp, 1.0);"
+        "}";
+
+    const char* fragment_shader =
+        "#version 400\n"
+        "out vec4 frag_colour;"
+        "void main() {"
+        "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+        "}";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        // wipe the drawing surface clear
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
+        // draw points 0-3 from the currently bound VAO with current in-use shader
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        // update other events like input handling
+        glfwPollEvents();
+        // put the stuff we've been drawing onto the display
+        glfwSwapBuffers(window);
+    }
+
+    // close GL context and any other GLFW resources
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
